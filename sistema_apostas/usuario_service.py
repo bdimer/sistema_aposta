f16802730244598292bb73af24377c36b9342927
bdimer <brunodimer.tc@gmail.com>
2026-06-11 17:48:23 -0300

    Update usuario_service.py

--- sistema_apostas/usuario_service.py
+++ sistema_apostas/usuario_service.py
@@ -1,13 +1,41 @@
 #validar_idade()
 #validar_senha()
 #cadastrar_usuario()
+from usuario import Usuario
 
 def validar_idade(data_nascimento):
     pass
 
-
 def validar_senha(senha):
-    pass
+
+    if len(senha) < 8:
+        return False
+
+    tem_maiuscula = False
+    tem_minuscula = False
+    tem_numero = False
+    tem_especial = False
+
+    for caractere in senha:
+
+        if caractere.isupper():
+            tem_maiuscula = True
+
+        elif caractere.islower():
+            tem_minuscula = True
+
+        elif caractere.isdigit():
+            tem_numero = True
+
+        else:
+            tem_especial = True
+
+    return (
+        tem_maiuscula and
+        tem_minuscula and
+        tem_numero and
+        tem_especial
+    )
 
 
 def verificar_cpf(cpf, usuarios):
