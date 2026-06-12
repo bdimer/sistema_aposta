367f3231497cc3e77f803874466841f211090ed8
bdimer <brunodimer.tc@gmail.com>
2026-06-12 20:57:09 -0300

    Update teste.py

--- sistema_apostas/teste.py
+++ sistema_apostas/teste.py
@@ -8,3 +8,11 @@ from usuario_service import validar_idade
 print(validar_idade("15/03/2000"))
 print(validar_idade("01/01/2015"))
 print(validar_idade("abc"))
+
+
+try:
+    idade = int(input("Digite sua idade: "))
+    print(f"Sua idade é {idade}")
+
+except ValueError:
+    print("Você deve digitar apenas números.")
