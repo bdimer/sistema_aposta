9ec353b8062ea35e5f78a604e55a3c2dcfebf517
bdimer <brunodimer.tc@gmail.com>
2026-06-13 18:54:12 -0300

    Update main.py

--- sistema_apostas/main.py
+++ sistema_apostas/main.py
@@ -1,16 +1,47 @@
 #CAMADA DE APRESENTAÇÃO
 
 from usuario import Usuario
+from usuario_service import cadastrar_usuario
 
-usuario = Usuario(
-    nome="João",
-    email="joao@email.com",
-    cpf="12345678900",
-    data_nascimento="01/01/1990",
-    login="joao123",
-    senha="Senha@123"
-)
+usuarios = []
 
-print(usuario.nome)
-print(usuario.pontos)
-print(usuario.status)
+while True:
+    print("\n===== SISTEMA DE APOSTAS =====")
+    print("1 - Cadastrar usuário")
+    print("2 - Sair")
+
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        nome = input("Nome: ")
+        email = input("Email: ")
+        cpf = input("CPF: ")
+        data_nascimento = input(
+            "Data de nascimento (dd/mm/aaaa): "
+        )
+        login = input("Login: ")
+        senha = input("Senha: ")
+
+        sucesso, resultado = cadastrar_usuario(
+            nome,
+            email,
+            cpf,
+            data_nascimento,
+            login,
+            senha,
+            usuarios
+        )
+        if sucesso:
+            print("Cadastro realizado com sucesso!")
+            print(resultado)
+        else:
+            print(resultado)
+
+
+    elif opcao == "2":
+        print("Encerrando sistema.")
+        break
+
+
+    else:
+        print("Opção inválida.")
