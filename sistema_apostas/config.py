b7d42e3b462381b24d8fc855f9c2f160762894b0
bdimer <brunodimer.tc@gmail.com>
2026-07-14 11:47:38 -0300

    Create config.py

---
+++ sistema_apostas/config.py
@@ -0,0 +1,12 @@
+#Apenas configurações do sistema, guarda informações que podem mudar
+#Constantes
+
+API_KEY = "9e1897bf"
+
+BASE_URL = "https://api.football-data.org/v4"
+
+HEADERS = {
+    "X-Auth-Token": API_KEY
+}
+
+ENDPOINT_MATCHES = "/competitions/WC/matches"
