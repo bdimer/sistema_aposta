43f2f092fff7d50e24db140ba60518b02e29439c
bdimer <brunodimer.tc@gmail.com>
2026-07-16 10:52:26 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+#Apenas configurações do sistema, guarda informações que podem mudar
+#Constantes
+
+API_KEY = "9e1897bf"
+
+BASE_URL = "https://api.football-data.org/v4"
+
+ENDPOINT_MATCHES = "/competitions/WC/matches"
+
+HEADERS = {
+    "X-Auth-Token": API_KEY
+}
+
