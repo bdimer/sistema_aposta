1fd575f18b1a43e10e0b84c664c79f1e8f470b40
bdimer <brunodimer.tc@gmail.com>
2026-07-28 16:13:14 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 #Apenas configurações do sistema, guarda informações que podem mudar
 #Constantes
 
+import os   # os permite acessar recursos do sistema operacional
+from dotenv import load_dotenv  #importa a função que lê o .env
 
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
 
 BASE_URL = "https://api.football-data.org/v4"
 
