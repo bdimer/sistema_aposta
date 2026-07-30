18e47cd94c49f153653dfc62521a37483eb5d29c
bdimer <brunodimer.tc@gmail.com>
2026-07-30 17:23:49 -0300

    Create aposta.py

---
+++ aposta.py
@@ -0,0 +1,26 @@
+
+class Aposta:
+    def __init__(
+            self,
+            id_aposta,
+            usuario,
+            partida,
+            gols_home,
+            gols_away,
+            pontos=0
+    ):
+        self.id_aposta = id_aposta
+        self.usuario = usuario
+        self.partida = partida
+        self.gols_home = gols_home
+        self.gols_away = gols_away
+        self.pontos = pontos
+
+    def __str__(self):
+        return (
+            f"Aposta: {self.id_aposta}\n"
+            f"Usuário: {self.usuario.nome}\n"
+            f"Partida: {self.partida.home_team} x {self.partida.away_team}\n"
+            f"Palpite: {self.gols_home} x {self.gols_away}\n"
+            f"Pontos: {self.pontos}"
+        )
